324f9afa401188407afe101c1473d62706b5510f
chen181390 <1813906834@qq.com>
2019-02-17 19:47:11 +0800

    增加正交相机的屏幕适配

--- Assets/Script/Character/CharacterCamera.cs
+++ Assets/Script/Character/CharacterCamera.cs
@@ -14,21 +14,29 @@ public class CharacterCamera : MonoBehaviour
     void Start()
     {
         this.characterCamera = this.GetComponent<Camera>();
-        this.characterCamera.orthographicSize = Camera.main.orthographicSize * 1920 / 1080 * Screen.height / Screen.width;
-        this.cameraCharacterDistance = (0.5f - 0.382f) * this.characterCamera.orthographicSize*2;
-        this.setCameraPos();
+        float defaultSize = this.characterCamera.orthographicSize;
+        this.characterCamera.orthographicSize = (float)Screen.height / 100 / 2;
+
+        this.startPosLimit += this.characterCamera.orthographicSize - defaultSize;
+        this.endPosLimit -= this.characterCamera.orthographicSize - defaultSize;
+
+        this.cameraCharacterDistance = (0.5f - 0.382f) * this.characterCamera.orthographicSize * 2;
+        this.transform.Translate(new Vector2(this.startPosLimit - this.transform.position.x, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.setCameraPos();
-    }
-
-    private void setCameraPos()
-    {
         float deltaMove = this.character.transform.position.x - this.transform.position.x + this.cameraCharacterDistance;
-        if (this.transform.position.x + deltaMove >= this.startPosLimit && this.transform.position.x + deltaMove <= this.endPosLimit)
+        if (this.transform.position.x + deltaMove <= this.startPosLimit)
+        {
+            this.transform.Translate(new Vector2(this.startPosLimit - this.transform.position.x, 0));
+        }
+        else if (this.transform.position.x + deltaMove > this.endPosLimit)
+        {
+            this.transform.Translate(new Vector2(this.endPosLimit - this.transform.position.x, 0));
+        }
+        else
         {
             this.transform.Translate(new Vector2(deltaMove, 0));
         }

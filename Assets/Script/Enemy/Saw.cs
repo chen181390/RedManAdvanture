6c1e8d6073ae8d77082dde90df730044b6799723
chen181390 <1813906834@qq.com>
2019-02-19 22:55:40 +0800

    优化锯齿轮遇到障碍物转向的逻辑

--- Assets/Script/Enemy/Saw.cs
+++ Assets/Script/Enemy/Saw.cs
@@ -3,26 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum SawType
 {
-    HorMoveUntilBlock,
-    VerMoveUntilBlock
+    MoveUntilBlock,
 }
 public class Saw : MonoBehaviour
 {
-    public Vector2 direct;
+    public Vector2 iniDirect;
     public SawType sawType;
     public float speed;
     private Vector3 iniPos;
     private CharacterBehaviour character;
+    private float sawRadius;
+    private GameObject checkBlockPoint1;
+    private GameObject checkBlockPoint2;
+    private Vector2 direct;
 
 
     // Start is called before the first frame update
     void Start()
     {
         this.iniPos = this.transform.position;
-        this.direct = this.direct.normalized;
+        this.iniDirect = this.iniDirect.normalized;
+        this.direct = this.iniDirect;
         this.character = GameObject.Find("Character").GetComponent<CharacterBehaviour>();
         var rigidbody = this.GetComponent<Rigidbody2D>();
         rigidbody.gravityScale = 0;
+        this.sawRadius = this.GetComponent<CircleCollider2D>().radius;
+        this.checkBlockPoint1 = GameObject.Find("checkBlockPoint1");
+        this.checkBlockPoint2 = GameObject.Find("checkBlockPoint2");
+
     }
 
     // Update is called once per frame
@@ -30,7 +38,20 @@ public class Saw : MonoBehaviour
     {
         switch(this.sawType)
         {
-            case SawType.HorMoveUntilBlock:
+            case SawType.MoveUntilBlock:
+                RaycastHit2D hit;
+                if (direct.Equals(this.iniDirect))
+                {
+                    hit = Physics2D.Raycast(this.checkBlockPoint1.transform.position, this.direct, 0.1f);
+                }
+                else
+                {
+                    hit = Physics2D.Raycast(this.checkBlockPoint2.transform.position, this.direct, 0.1f);
+                }
+                if (hit.transform)
+                {
+                    this.direct = - this.direct;
+                }
                 this.transform.Translate(this.direct * Time.deltaTime * speed);
                 break;
         }
@@ -44,6 +65,5 @@ public class Saw : MonoBehaviour
             return;
         }
 
-        this.direct = - this.direct;
     }
 }
